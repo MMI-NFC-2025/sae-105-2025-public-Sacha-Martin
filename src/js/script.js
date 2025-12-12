363cc391f86b4040380885fa9b6174e1b51fa6df
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2025-12-12 09:27:42 +0100

    debut js

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const logo = document.querySelector(".header__logo");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+
+    // Mise à jour des attributs ARIA pour accessibilité
+    toggle.ariaExpanded = isClosed;
+    nav.hidden = isOpen;
+    logo.classList.toggle("header__logo--extend", isClosed);
+    page.classList.toggle("u-noscroll", isClosed);
+  });
+}
